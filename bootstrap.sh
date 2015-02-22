987812b442d8fe03352a8a78f4451385f8924090
The Dude <u6du65@gmail.com>
2015-02-22 15:28:30 +0200

    Added the installation of nodejs, npm, coffeescript, gulp to the bootstrap script.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,19 @@
 
 sudo apt-get update
 sudo apt-get install -y git
+sudo apt-get install -y nodejs
+sudo apt-get install -y npm
+
+sudo npm install -g coffee-script
+sudo npm install -g gulp
 
 cd /vagrant
+
+# - Clone the github repo
+# - Copy over the frameworks directory
+# - Install the nodejs, npm
+# - Install CoffeeScript
+# - Install Gulp
+# - Run the build script for the coffeescript.
 
 git clone https://github.com/6D65/shovelleros
