9cd5ee898f6a2bce240f98c89e5b4cd917bf0386
The Dude <u6du65@gmail.com>
2015-02-25 23:33:35 +0200

    Updated the boostrap file, to actually setup the shovelleros repo.

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,8 @@ sudo apt-get install -y git
 sudo apt-get install -y nodejs
 sudo apt-get install -y npm
 
+sudo npm config set registry http://registry.npmjs.org/
+
 sudo npm install -g coffee-script
 sudo npm install -g gulp
 
@@ -18,3 +20,10 @@ cd /vagrant
 # - Run the build script for the coffeescript.
 
 git clone https://github.com/6D65/shovelleros
+
+source ~/.bashrc
+
+sudo cocos new dummy -l js -d /vagrant
+
+# sudo cp -r /vagrant/dummy/dummy/frameworks /vagrant/shovelleros/frameworks
+sudo mv /vagrant/dummy/dummy/frameworks /vagrant/shovelleros/frameworks
